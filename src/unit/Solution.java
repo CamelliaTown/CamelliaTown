9aca9a82be5aede7bf48c7482058eb7dd195ce92
CamelliaTown <TadSo116@outlook.com>
2020-11-25 08:03:01 +0800

    空间复杂度为O(1)，对数组去重的算法

--- src/unit/Solution.java
+++ src/unit/Solution.java
@@ -3,7 +3,7 @@ package unit;
 import java.util.Arrays;
 
 public class Solution {
-
+    //方法二
     public static int removeDuplicates(int[] nums){
         int n = -1;
         int length = nums.length;
@@ -27,11 +27,23 @@ public class Solution {
         }
         return length;
     }
-
+    //方法一
+    public static int removeDuplicates2(int[] nums){
+        int length=0;
+        int num=-1;
+        for (int i = 0; i < nums.length; i++) {
+            if(num!=nums[i]){
+                num = nums[i];
+                nums[length] = nums[i];
+                length++;
+            }
+        }
+        return length;
+    }
 
     public static void main(String[] args) {
-        int[] list = new int[]{0,0,1,1,1,1,1,1,1,2,2,3,3,4};
-        int num = removeDuplicates(list);
+        int[] list = new int[]{0,1,1,1,1,1,2,3,4,5,5,5,5,5};
+        int num = removeDuplicates2(list);
         System.out.println("num="+num);
         System.out.println(Arrays.toString(list));
     }
